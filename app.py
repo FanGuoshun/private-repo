d66e8ebd2c53fd714752a94308d2614dadb2c40c
FanGuoshun <30681858+FanGuoshun@users.noreply.github.com>
2023-03-08 10:49:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from transformers import pipeline
 import gradio as gr
-
+import logging
 
 def hello(i):
     classifier = pipeline("sentiment-analysis")
     a = classifier(i)
+    logging.info('文本分类结束')
     return a
-
-
-
-
-
-
-
-
 
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
