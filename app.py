05d4b936304ffc7f0c9a1240a9d49e48239ce297
FanGuoshun <30681858+FanGuoshun@users.noreply.github.com>
2023-03-31 16:30:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ def hello(i):
     classifier = pipeline("sentiment-analysis")
     a = classifier(i)
     logging.warning('文本分类结束')
-    asdadsasadsa
     return a
-
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch()
