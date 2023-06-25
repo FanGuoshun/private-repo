f264dfd9391c036c1c6f9bd3289e0787886aa00c
fanguoshun <30681858+FanGuoshun@users.noreply.github.com>
2023-06-25 15:12:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from transformers import pipeline
 import gradio as gr
 import logging
+import requests
+import os
+
 
 def hello(i):
     logging.warning('这是test3分支')
+    url = "https://huggingface.co"
+    response = requests.get(url)
+    print(str(os.environ.get("http_proxy")))
+    print(str(os.environ.get("https_proxy")))
+    print(str(os.environ.get("no_proxy")))
+    print(str(os.environ.get("PATH")))
+    print("Request:")
+    print(response.request.headers)
+    print("\nResponse:")
+    print(response.status_code)
+    print(response.headers)
     return "这是test3分支测试更新11111111111"
-
 
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
