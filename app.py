ec30461c830f3d79dbadc2747f58318d6b1d35e3
fanguoshun <30681858+FanGuoshun@users.noreply.github.com>
2024-02-20 13:43:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,23 @@ import gradio as gr
 import logging
 
 def hello(i):
-    logging.warning('这是test2分支')
+    logging.warning('这是test2分支1')
+    logging.warning('这是test2分支2')
+    logging.warning('这是test2分支3')
+    logging.warning('这是test2分支4')
+    logging.warning('这是test2分支5')
+    logging.warning('这是test2分支6')
+    logging.warning('这是test2分支7')
+    logging.warning('这是test2分支8')
+    logging.warning('这是test2分支9')
+    logging.warning('这是test2分支10')
+    logging.warning('这是test2分支11')
+    logging.warning('这是test2分支12')
+    logging.warning('这是test2分支13')
+    logging.warning('这是test2分支14')
+    logging.warning('这是test2分支15')
+    logging.warning('这是test2分支16')
+
     return "这是test2分支测试更新"
 
 
