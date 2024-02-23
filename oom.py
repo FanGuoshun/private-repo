397feea19a97a1e5d8486c68d09e667c65268403
fanguoshun <30681858+FanGuoshun@users.noreply.github.com>
2024-02-23 10:30:27 +0800

    Create oom.py

---
+++ oom.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+import gradio as gr
+import logging
+
+def hello(i):
+     # 尝试创建一个巨大的numpy数组
+    huge_array = np.zeros(10**9, dtype=np.int64)  # 分配约10GB内存
+    logging.warning(i)
+
+    return "这是test2分支测试更新"
+
+
+
+iface = gr.Interface111(fn=hello, inputs="text", outputs="text")
+iface.launch()
