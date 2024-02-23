53ad576d7f9eb950678d6d98b5bed58deb3dad9e
fanguoshun <30681858+FanGuoshun@users.noreply.github.com>
2024-02-23 10:40:57 +0800

    Update oom.py

--- oom.py
+++ oom.py
@@ -2,11 +2,10 @@ from transformers import pipeline
 import gradio as gr
 import logging
 import numpy as np
-
+import array
 
 def hello(i):
-     # 尝试创建一个巨大的numpy数组
-    huge_array = np.zeros(10**9, dtype=np.int64)  # 分配约10GB内存
+    huge_array = array.array('i', [0] * (10**9))  # 分配约10GB内存
     logging.warning(i)
 
     return "这是test2分支测试更新"
